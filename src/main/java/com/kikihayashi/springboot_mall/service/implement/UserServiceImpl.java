dcfbd005d4100fdac3e6faf5b5f34229d9f84c82
kikihayashi <r05522504@g.ntu.edu.tw>
2022-10-07 18:23:43 +0800

    2022/10/07 帳號功能-使用MD5加密密碼

--- src/main/java/com/kikihayashi/springboot_mall/service/implement/UserServiceImpl.java
+++ src/main/java/com/kikihayashi/springboot_mall/service/implement/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -29,6 +30,10 @@ public class UserServiceImpl implements UserService {
             log.warn("該email {} 已被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+
+        //使用MD5 生成雜湊密碼(Hash)
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
         //建立帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -48,7 +53,10 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+        //轉換登入密碼為 MD5 (hash)
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+        //驗證密碼是否相同
+        if (user.getPassword().equals(hashedPassword)) {
             return user;
         } else {
             log.warn("該Email {} 密碼不正確", userLoginRequest.getEmail());
